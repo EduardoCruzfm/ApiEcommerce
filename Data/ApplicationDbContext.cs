8260b7a42cd23e32ddebbad0ecd1dfd87733bed5
eduardo <eduardofrankcruzmendez@gmail.com>
2025-10-25 11:20:39 -0300

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+
+    // Representación una tabla de categorías en la base de datos
+    // por convención, el nombre de la tabla será "Categories", en plural
+    public DbSet<Category> Categories { get; set; }
+}
