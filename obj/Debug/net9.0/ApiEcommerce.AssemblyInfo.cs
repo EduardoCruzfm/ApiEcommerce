38da54040e70898d591824c2089cf9a3f0497cee
eduardo <eduardofrankcruzmendez@gmail.com>
2025-11-11 20:30:26 -0300

    feat(models): add Product model

--- obj/Debug/net9.0/ApiEcommerce.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25f16cf3eb7331529f769319ad0ee220a9316366")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d927894850541c3c3349c812232b4a8993415f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
