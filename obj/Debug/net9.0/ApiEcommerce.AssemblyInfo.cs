093b6abd4227b73a6f94ef4e2ae6c7351831d285
eduardo <eduardofrankcruzmendez@gmail.com>
2025-11-12 20:30:50 -0300

    feat(models): add Product DTOs

--- obj/Debug/net9.0/ApiEcommerce.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38da54040e70898d591824c2089cf9a3f0497cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+123dc651d27b9758a5f847811319af7e4fdb7764")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
